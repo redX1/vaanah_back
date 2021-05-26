c612c3f07563cd5116aeeadd903fd99f74f253ea
Fatma Gaye LO <FGLO>
2021-05-26 10:56:20 +0000

    fix: vaana-428-uuid-field

--- vaana_app/stores/urls.py
+++ vaana_app/stores/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
   path('stores', views.StoreAPIView.as_view()),
-  path('stores/<int:store_id>', views.RetrieveDeleteUpdateStoreAPIView.as_view()),
-  path('stores/<int:store_id>/products', views.StoreProductsAPIView.as_view()),
-  path('stores-activated', views.StoreActivatedAPIView.as_view()),
-  path('stores-deactivated', views.StoreDeactivatedAPIView.as_view()),
-  path('stores-latest', views.LatestStoreAPIView.as_view()),
+  path('stores/<uuid:store_id>', views.RetrieveDeleteUpdateStoreAPIView.as_view()),
+  path('stores/<uuid:store_id>/products', views.StoreProductsAPIView.as_view()),
+  path('stores/activated', views.StoreActivatedAPIView.as_view()),
+  path('stores/deactivated', views.StoreDeactivatedAPIView.as_view()),
+  path('stores/latest', views.LatestStoreAPIView.as_view()),
 ]

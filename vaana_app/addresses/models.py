02a814e27c4ca38dd31ebfdae9e463c58c4fc5e2
Alioune Kanoute <alioune.kanoute@xegit.com>
2021-05-27 11:02:55 +0000

    feat(#450): Shopping cart(Create and get cart, add product to cart) feat(#427): Order application created feat(#454): Shiiping Address model added feat(#457): Address added

---
+++ vaana_app/addresses/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from cores.models import TimestampedModel
+import uuid
+
+class Address(TimestampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    state = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=64, blank=True)
+    country = models.CharField(max_length=255)
--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'stores',
     'carts',
     'orders',
+    'addresses',
 ]
 
 MIDDLEWARE = [

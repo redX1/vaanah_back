8492a895fe812b00916349edf34666bbf6d85376
Fatma Gaye LO <FGLO>
2021-05-28 15:19:03 +0000

    fix : users endpoints

--- vaana_app/users/urls.py
+++ vaana_app/users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^users/login/?$', LoginAPIView.as_view()),
     path('users/verify/', VerifyEmail.as_view(), name="email-verify"),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/reset/email/', RequestPasswordResetEmail.as_view(), name="request-reset-email"),
+    path('users/password/reset/', RequestPasswordResetEmail.as_view(), name="request-reset-email"),
     path('users/password/reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('users/password/reset/complete/', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
     

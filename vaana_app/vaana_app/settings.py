32c39fa47847d4f3013d959e656070da3876a28e
Fatma Gaye LO <FGLO>
2021-06-04 16:15:03 +0000

    fix: authenfication v5

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vaanahdbfatma2',
+        'NAME': 'postgres',
         'USER': 'vaanah_user',
         'PASSWORD': 'secret',
         'HOST': '3.122.225.2',

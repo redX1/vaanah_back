e0a0178346207e0bc9784be7f14f98dd4c74a55c
Alioune Kanoute <alioune.kanoute@xegit.com>
2021-07-09 17:48:14 +0000

    release: database access updated

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'vaanahdb2',
         'USER': 'vaanah_user',
         'PASSWORD': 'secretsecret',
         'HOST': 'vaanahdb.cvamgenajfwz.eu-central-1.rds.amazonaws.com',

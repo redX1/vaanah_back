8cbbb878c441c2ee0b8bf946f0bbd9a591a8c65e
Alioune Kanoute <alioune.kanoute@xegit.com>
2021-07-23 11:13:19 +0000

    feature: funds and wallets models added

--- vaana_app/funds/models.py
+++ vaana_app/funds/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from cores.models import TimestampedModel
+import uuid
+from django.conf import settings
+from orders.models import Order
+from payments.models import PaymentModel
+from wallets.models import Wallet
 
-# Create your models here.
+class Fund(TimestampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.DecimalField(decimal_places=2, max_digits=12)
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    payment = models.ForeignKey(PaymentModel, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+    COLLECTED, REFUNDED, TRANSFERED = (
+        "collected", "refunded", "transfered"
+    )
+
+    STATUS = [
+        (COLLECTED, "collected"),
+        (REFUNDED, "refunded"),
+        (TRANSFERED, "trensfered")
+    ]
+
+    status = models.CharField(max_length=100, default=COLLECTED, choices=STATUS)
+
--- vaana_app/wallets/models.py
+++ vaana_app/wallets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from cores.models import TimestampedModel
+import uuid
+from django.conf import settings
 
-# Create your models here.
+class Wallet(TimestampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)

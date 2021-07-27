9f15f8ecae73c18063acddf0fd89f0ecc25be8fb
Pape Alioune <badara.diakhate@xegit.com>
2021-07-27 10:45:45 +0000

    Setup test stage on workflow

--- vaana_app/start-test.sh
+++ vaana_app/start-test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # start-test.sh
 
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py test

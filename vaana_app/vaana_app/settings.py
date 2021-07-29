2edddaf9f6d55dc20a215cf6445cc6d6b1f1fe25
Pape Alioune <badara.diakhate@xegit.com>
2021-07-29 16:18:25 +0000

    Update workflow for test

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'develop',
         'USER': 'vaanah_user',
         'PASSWORD': 'secretsecret',
         'HOST': 'vaanahdb.cvamgenajfwz.eu-central-1.rds.amazonaws.com',

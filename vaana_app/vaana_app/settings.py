92ef2f0f6ac6e8a43b59ff1211df20799ed4051b
Pape Alioune <badara.diakhate@xegit.com>
2021-08-25 14:37:56 +0000

    Update database settings for deployment

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'release0',
         'USER': 'vaanah_user',
         'PASSWORD': 'secretsecret',
         'HOST': 'vaanahdb.cvamgenajfwz.eu-central-1.rds.amazonaws.com',

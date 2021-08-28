3f88ece3ee0b4fbcde98602a651fee1b38cf1dda
Pape Alioune <badara.diakhate@xegit.com>
2021-08-28 09:38:42 +0000

    Unit test and integration test

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chatbottest',
+        'NAME': 'release0',
         'USER': 'vaanah_user',
         'PASSWORD': 'secretsecret',
         'HOST': 'vaanahdb.cvamgenajfwz.eu-central-1.rds.amazonaws.com',

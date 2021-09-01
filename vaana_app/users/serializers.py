eed61f1810c1366303eccd7fef1f2ee9b4572dfb
Fatma Gaye LO <FGLO>
2021-09-01 11:29:11 +0000

    refactor seller register

--- vaana_app/users/serializers.py
+++ vaana_app/users/serializers.py
@@ -11,6 +11,30 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from addresses.serializers import AddressSerializer
 
+class SellerRegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+  
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the client.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+        fields = ('id', 'email', 'username', 'password', 'token', 'is_verified', 'account_type', 'gender', 'address')
+
+    def create(self, validated_data):
+        # Use the `create_user` method we wrote earlier to create a new user.
+        return User.objects.create_user(**validated_data)
 class RegistrationSerializer(serializers.ModelSerializer):
     """Serializers registration requests and creates a new user."""
   
--- vaana_app/users/urls.py
+++ vaana_app/users/urls.py
@@ -1,26 +1,26 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import (
-    LoginAPIView, PasswordTokenCheckAPI, RegistrationAPIView, ResendEmailAPI, RequestPasswordResetEmail, SetNewPasswordAPIView, SingleUserAPIView, UserRetrieveUpdateAPIView, VerifyEmail, UserStoreAPIView, UserAPIView
-)
+from . import views
+
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
 urlpatterns = [
-    url(r'^users/?$', UserRetrieveUpdateAPIView.as_view() ),
-    url(r'^users/register/?$', RegistrationAPIView.as_view(), name="register"),
-    url(r'^users/login/?$', LoginAPIView.as_view(), name="login"),
-    path('users/verify/', VerifyEmail.as_view(), name="email-verify"),
-    path('users/resend/', ResendEmailAPI.as_view(), name="email-resend"),
+    url(r'^users/?$', views.UserRetrieveUpdateAPIView.as_view() ),
+    url(r'^users/register/?$', views.RegistrationAPIView.as_view(), name="register"),
+    url(r'^users/seller-register/?$', views.SellerRegistrationAPIView.as_view(), name="seller-register"),
+    url(r'^users/login/?$', views.LoginAPIView.as_view(), name="login"),
+    path('users/verify/', views.VerifyEmail.as_view(), name="email-verify"),
+    path('users/resend/', views.ResendEmailAPI.as_view(), name="email-resend"),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/password/reset/', RequestPasswordResetEmail.as_view(), name="request-reset-email"),
-    path('users/password/reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
-    path('users/password/reset/complete/', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
+    path('users/password/reset/', views.RequestPasswordResetEmail.as_view(), name="request-reset-email"),
+    path('users/password/reset/<uidb64>/<token>/', views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('users/password/reset/complete/', views.SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
     
-    path('users/<int:user_id>/stores', UserStoreAPIView.as_view()),
-    path('users/all', UserAPIView.as_view()),
-    path('users/<int:user_id>', SingleUserAPIView.as_view()),
+    path('users/<int:user_id>/stores', views.UserStoreAPIView.as_view()),
+    path('users/all', views.UserAPIView.as_view()),
+    path('users/<int:user_id>', views.SingleUserAPIView.as_view()),
 
 ]

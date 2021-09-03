a82f629bd6bc284fd05c2e5346c8acd702d59d25
Pape Alioune <badara.diakhate@xegit.com>
2021-09-03 17:20:07 +0000

    Deploy

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -117,6 +117,7 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',

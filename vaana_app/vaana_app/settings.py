08ab58123fe3e2554205ddeeb0066aa7025c3cae
Fatma Gaye LO <FGLO>
2021-09-10 12:28:47 +0000

    integrate social_auth 2

--- vaana_app/vaana_app/settings.py
+++ vaana_app/vaana_app/settings.py
@@ -124,11 +124,10 @@ WSGI_APPLICATION = 'vaana_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres7',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'vaanah_user',
+        'PASSWORD': 'secretsecret',
+        'HOST': 'vaanahdb.cvamgenajfwz.eu-central-1.rds.amazonaws.com',
     }
 }
 

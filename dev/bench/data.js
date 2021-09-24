1115bff91d0834e77db9b73889572a40d91d7ca9
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 04:03:48 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 9bbc62f18bb922bfd2cc1cab39d33d5ba8277423

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632455406034,
+  "lastUpdate": 1632456228055,
   "repoUrl": "https://github.com/redX1/vaanah_back",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -38,6 +38,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0003684073155803023",
             "extra": "mean: 2.6481941195335525 msec\nrounds: 343"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alioune.kanoute@terinnova.com",
+            "name": "aliouneKanouteTerinnova",
+            "username": "aliouneKanouteTerinnova"
+          },
+          "committer": {
+            "email": "alioune.kanoute@terinnova.com",
+            "name": "aliouneKanouteTerinnova",
+            "username": "aliouneKanouteTerinnova"
+          },
+          "distinct": true,
+          "id": "9bbc62f18bb922bfd2cc1cab39d33d5ba8277423",
+          "message": "up",
+          "timestamp": "2021-09-24T04:03:19Z",
+          "tree_id": "9ef1209670a54ffedb6f579d0c076cec71531981",
+          "url": "https://github.com/redX1/vaanah_back/commit/9bbc62f18bb922bfd2cc1cab39d33d5ba8277423"
+        },
+        "date": 1632456226061,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 53676.57771042441,
+            "unit": "iter/sec",
+            "range": "stddev: 4.492133100329017e-7",
+            "extra": "mean: 18.630099806191485 usec\nrounds: 36631"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 446.21227844228736,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000008581377422285487",
+            "extra": "mean: 2.241085797752961 msec\nrounds: 445"
+          },
+          {
+            "name": "bench.py::test_fib_30",
+            "value": 3.6372052565623423,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002208349497213322",
+            "extra": "mean: 274.93636720000154 msec\nrounds: 5"
           }
         ]
       }

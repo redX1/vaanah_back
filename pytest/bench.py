02f9797b4d6690068df0da8c284ca5ef49a43dc2
aliouneKanouteTerinnova <alioune.kanoute@terinnova.com>
2021-09-24 03:57:52 +0000

    yesso

--- pytest/bench.py
+++ pytest/bench.py
@@ -1,8 +1,14 @@
 from fib import fib
 import pytest
 
+
 def test_fib_10(benchmark):
     benchmark(fib, 10)
 
+
 def test_fib_20(benchmark):
     benchmark(fib, 20)
+
+
+def test_fib_60(benchmark):
+    benchmark(fib, 60)

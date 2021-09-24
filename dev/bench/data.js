f3d8c9a803d7b045312edd07ef1d42764c03a031
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 03:50:06 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for d34e803ee4e94ec46a23e945fd937ddd3775bf44

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632455406034,
+  "repoUrl": "https://github.com/redX1/vaanah_back",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "alioune.kanoute@terinnova.com",
+            "name": "aliouneKanouteTerinnova",
+            "username": "aliouneKanouteTerinnova"
+          },
+          "committer": {
+            "email": "alioune.kanoute@terinnova.com",
+            "name": "aliouneKanouteTerinnova",
+            "username": "aliouneKanouteTerinnova"
+          },
+          "distinct": true,
+          "id": "d34e803ee4e94ec46a23e945fd937ddd3775bf44",
+          "message": "remove push auto",
+          "timestamp": "2021-09-24T03:49:37Z",
+          "tree_id": "948fcff1740619fc3e8b4bb238b13253845d7fb8",
+          "url": "https://github.com/redX1/vaanah_back/commit/d34e803ee4e94ec46a23e945fd937ddd3775bf44"
+        },
+        "date": 1632455405054,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 44053.0881921195,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00007810692173845592",
+            "extra": "mean: 22.69988418607362 usec\nrounds: 31447"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 377.61582227821646,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003684073155803023",
+            "extra": "mean: 2.6481941195335525 msec\nrounds: 343"
+          }
+        ]
+      }
+    ]
+  }
+}
